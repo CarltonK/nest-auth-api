04f8bc87d5e853c3bfd48a46aecef00ec90eabe8
*vfWi4qyy5k <mcarlton33@gmail.com>
2025-04-19 16:54:44 +0300

    Use appropriate function name

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly _appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this._appService.getHello();
+  livenessCheck(): string {
+    return this._appService.livenessCheck();
   }
 }
